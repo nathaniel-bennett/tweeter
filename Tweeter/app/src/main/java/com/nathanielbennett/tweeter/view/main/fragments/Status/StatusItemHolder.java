a35f0a6394f60fc0d142f85cb682853e6d75698f
csmcclain <christopher.mcclain@icloud.com>
2021-02-22 18:13:41 -0700

    Added click on userMention functionality

--- Tweeter/app/src/main/java/com/nathanielbennett/tweeter/view/main/fragments/Status/StatusItemHolder.java
+++ Tweeter/app/src/main/java/com/nathanielbennett/tweeter/view/main/fragments/Status/StatusItemHolder.java
@@ -2,6 +2,10 @@ package com.nathanielbennett.tweeter.view.main.fragments.Status;
 
 import android.content.Context;
 import android.content.Intent;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -24,6 +28,8 @@ public class StatusItemHolder extends TemplateItemHolder<Status> {
     private final TextView postDateInfo;
     private final TextView statusMessage;
     private User associatedUser;
+    private Status associatedStatus;
+    private Context associatedContext;
 
     /**
      * Creates an instance.
@@ -32,6 +38,7 @@ public class StatusItemHolder extends TemplateItemHolder<Status> {
      */
     public StatusItemHolder(@NonNull View itemView, int viewType, Context context) {
         super(itemView);
+        this.associatedContext = context;
 
         if (viewType == STORY_VIEW_TYPE) {
             userImage = itemView.findViewById(R.id.userImage_Story);
@@ -60,16 +67,57 @@ public class StatusItemHolder extends TemplateItemHolder<Status> {
 
     @Override
     public void bindItem(Status itemToBind) {
+        associatedStatus = itemToBind;
         associatedUser = itemToBind.getUserOfStatus();
-        try {
-            userImage.setImageDrawable(ImageUtils.drawableFromByteArray(itemToBind.getUserOfStatus().getImageBytes()));
-            userAlias.setText(itemToBind.getUserOfStatus().getAlias());
-            userName.setText(itemToBind.getUserOfStatus().getName());
-            postDateInfo.setText(itemToBind.getDatePosted());
-            statusMessage.setText(itemToBind.getStatusMessage());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        userImage.setImageDrawable(ImageUtils.drawableFromByteArray(itemToBind.getUserOfStatus().getImageBytes()));
+        userAlias.setText(itemToBind.getUserOfStatus().getAlias());
+        userName.setText(itemToBind.getUserOfStatus().getName());
+        postDateInfo.setText(itemToBind.getDatePosted());
 
+        SpannableString message = getUserMentions(itemToBind.getStatusMessage());
+
+        statusMessage.setText(message);
+        statusMessage.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+
+    private SpannableString getUserMentions(String status) {
+        SpannableString ss = new SpannableString(status);
+
+        int currentAtSymbol = -1;
+        for (int i = 0; i < status.length(); i++) {
+            if (status.charAt(i) == '@') {
+                currentAtSymbol = i;
+            } else if (currentAtSymbol != -1 && (status.charAt(i) == ' ' || i == status.length() - 1)) {
+                int start = currentAtSymbol;
+                int end = (i == status.length() - 1) ? i + 1 : i;
+                ClickableSpan clickableSpan = new ClickableSpan() {
+                    @Override
+                    public void onClick(@NonNull View widget) {
+
+                        boolean activityMade = false;
+
+                        for (User user : associatedStatus.getMentions()) {
+                            if (user.getAlias().equals(status.substring(start, end))) {
+                                Intent intent = new Intent(associatedContext, UserActivity.class);
+                                intent.putExtra("user", user);
+                                associatedContext.startActivity(intent);
+                                activityMade = true;
+                                break;
+                            }
+                        }
+
+                        if (!activityMade) {
+                            Toast.makeText(associatedContext, "Cannot find associatedUser", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                };
+                ss.setSpan(clickableSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                currentAtSymbol = -1;
+            }
+        }
+        return ss;
     }
 }
