cd64f13da56edb4474ec6686c55bcd6fc45a7140
CJohnL <59421276+CJohnL@users.noreply.github.com>
2021-03-10 18:00:41 -0700

    Update Strategies

--- Tweeter/app/src/main/java/com/nathanielbennett/tweeter/client/model/net/webrequeststrategies/LoginStrategy.java
+++ Tweeter/app/src/main/java/com/nathanielbennett/tweeter/client/model/net/webrequeststrategies/LoginStrategy.java
@@ -1,4 +1,31 @@
 package com.nathanielbennett.tweeter.client.model.net.webrequeststrategies;
 
-public class LoginStrategy {
+import com.nathanielbennett.tweeter.client.model.net.ClientCommunicator;
+import com.nathanielbennett.tweeter.model.service.response.TweeterAPIResponse;
+
+public class LoginStrategy implements ClientCommunicator.WebRequestStrategy{
+    @Override
+    public String getRequestPath() {
+        return null;
+    }
+
+    @Override
+    public String getRequestMethod() {
+        return null;
+    }
+
+    @Override
+    public TweeterAPIResponse formResponse(String serializedResponse) {
+        return null;
+    }
+
+    @Override
+    public TweeterAPIResponse formFailureResponse(int httpResponseCode) {
+        return null;
+    }
+
+    @Override
+    public TweeterAPIResponse formIOErrorResponse(String message) {
+        return null;
+    }
 }
