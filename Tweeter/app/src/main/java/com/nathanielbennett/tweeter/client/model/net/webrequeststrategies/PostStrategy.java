9da215adc18dadd1c8ff00a86b4bd32bc853e53e
CJohnL <59421276+CJohnL@users.noreply.github.com>
2021-03-10 18:28:39 -0700

    Update Strategies

--- Tweeter/app/src/main/java/com/nathanielbennett/tweeter/client/model/net/webrequeststrategies/PostStrategy.java
+++ Tweeter/app/src/main/java/com/nathanielbennett/tweeter/client/model/net/webrequeststrategies/PostStrategy.java
@@ -1,26 +1,36 @@
 package com.nathanielbennett.tweeter.client.model.net.webrequeststrategies;
 
 import com.nathanielbennett.tweeter.client.model.net.ClientCommunicator;
+import com.nathanielbennett.tweeter.model.net.Serializer;
+import com.nathanielbennett.tweeter.model.service.response.PostResponse;
 import com.nathanielbennett.tweeter.model.service.response.TweeterAPIResponse;
 
 public class PostStrategy implements ClientCommunicator.WebRequestStrategy{
     @Override
     public String getRequestPath() {
-        return null;
+        return "/post";
     }
 
     @Override
     public String getRequestMethod() {
-        return null;
+        return "POST";
     }
 
     @Override
     public TweeterAPIResponse formResponse(String serializedResponse) {
-        return null;
+        Serializer serializer = new Serializer();
+        return serializer.deserialize(serializedResponse, PostResponse.class);
     }
 
     @Override
     public TweeterAPIResponse formFailureResponse(int httpResponseCode) {
-        return null;
+        switch (httpResponseCode) {
+            case 400:
+                return new PostResponse("Client error");
+            case 500:
+                return new PostResponse("Server error");
+            default:
+                return new PostResponse("An unknown error occurred");
+        }
     }
 }
