d64aedf5414e24c19a251388ec7b3f9a7948750c
Nathaniel <me@nathanielbennett.com>
2021-04-01 07:19:28 -0600

    Added Status object for internal use with database storage

---
+++ Tweeter/server/src/main/java/com/nathanielbennett/tweeter/server/model/StoredStatus.java
@@ -0,0 +1,40 @@
+package com.nathanielbennett.tweeter.server.model;
+
+public class StoredStatus {
+    private String alias;
+    private String message;
+    private String timestamp;
+
+    public StoredStatus(String alias, String message, String timestamp) {
+        this.alias = alias;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public StoredStatus() { }
+
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
