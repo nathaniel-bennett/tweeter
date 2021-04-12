59400b359e5a11ba31bf5da0bd7f590e1080f579
Nathaniel <me@nathanielbennett.com>
2021-04-12 13:20:10 -0600

    Finalized fixes in having messages sorted by timestamp rather than message contents

--- Tweeter/server/src/main/java/com/nathanielbennett/tweeter/server/dao/FeedDAO.java
+++ Tweeter/server/src/main/java/com/nathanielbennett/tweeter/server/dao/FeedDAO.java
@@ -15,9 +15,9 @@ public class FeedDAO extends AmazonDAOTemplate {
     private static final String TABLE_NAME = "feed";
 
     private static final String PARTITION_KEY_LABEL = "owner";
-    private static final String SORT_KEY_LABEL = "message";
+    private static final String SORT_KEY_LABEL = "timestamp";
     private static final String ALIAS_LABEL = "alias";
-    private static final String TIMESTAMP_LABEL = "date_posted";
+    private static final String MESSAGE_LABEL = "message";
 
 
     public FeedDAO() {
@@ -27,9 +27,9 @@ public class FeedDAO extends AmazonDAOTemplate {
     @Override
     protected Object databaseItemToObject(Map<String, AttributeValue> item) throws DataAccessException {
         // String feedOwner = item.get(PARTITION_KEY_LABEL).getS();
-        String message = item.get(SORT_KEY_LABEL).getS();
+        String timestamp = item.get(SORT_KEY_LABEL).getS();
         String alias = item.get(ALIAS_LABEL).getS();
-        String timestamp = item.get(TIMESTAMP_LABEL).getS();
+        String message = item.get(MESSAGE_LABEL).getS();
 
         return new StoredStatus(alias, message, timestamp);
     }
@@ -39,14 +39,14 @@ public class FeedDAO extends AmazonDAOTemplate {
         StoredStatus status = (StoredStatus) o;
 
         return new Item()
-                .withPrimaryKey(PARTITION_KEY_LABEL, status.getFeedOwner(), SORT_KEY_LABEL, status.getMessage())
+                .withPrimaryKey(PARTITION_KEY_LABEL, status.getFeedOwner(), SORT_KEY_LABEL, status.getTimestamp())
                 .withString(ALIAS_LABEL, status.getAlias())
-                .withString(TIMESTAMP_LABEL, status.getTimestamp());
+                .withString(MESSAGE_LABEL, status.getMessage());
     }
 
 
-    public List<StoredStatus> getUserFeed(String feedOwner, int numStatuses, String lastSeenStatus) {
-        ResultsPage resultsPage = getPagedFromDatabase(feedOwner, numStatuses, lastSeenStatus, false);
+    public List<StoredStatus> getUserFeed(String feedOwner, int numStatuses, String lastStatusTimestamp) {
+        ResultsPage resultsPage = getPagedFromDatabase(feedOwner, numStatuses, lastStatusTimestamp, false);
         if (!resultsPage.hasValues()) {
             return null;
         }
