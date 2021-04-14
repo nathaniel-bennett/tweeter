880741f5e3dd09cf0ada5e399060c397d1dc6dd9
csmcclain <christopher.mcclain@icloud.com>
2021-04-14 09:39:15 -0600

    Fixed Post service tests

--- Tweeter/app/src/test/java/com/nathanielbennett/tweeter/client/integrationTests/PostServiceTest.java
+++ Tweeter/app/src/test/java/com/nathanielbennett/tweeter/client/integrationTests/PostServiceTest.java
@@ -1,10 +1,14 @@
 package com.nathanielbennett.tweeter.client.integrationTests;
 
+import com.nathanielbennett.tweeter.client.model.service.LoginServiceProxy;
 import com.nathanielbennett.tweeter.client.model.service.PostServiceProxy;
 import com.nathanielbennett.tweeter.model.domain.AuthToken;
 import com.nathanielbennett.tweeter.model.net.TweeterRemoteException;
+import com.nathanielbennett.tweeter.model.service.LoginService;
 import com.nathanielbennett.tweeter.model.service.PostService;
+import com.nathanielbennett.tweeter.model.service.request.LoginRequest;
 import com.nathanielbennett.tweeter.model.service.request.PostRequest;
+import com.nathanielbennett.tweeter.model.service.response.LoginResponse;
 import com.nathanielbennett.tweeter.model.service.response.PostResponse;
 
 import org.junit.jupiter.api.Assertions;
@@ -20,9 +24,13 @@ public class PostServiceTest {
     private PostService postService;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws IOException, TweeterRemoteException
+    {
+        LoginService service = new LoginServiceProxy();
+        LoginResponse response = service.login(new LoginRequest("testUser", "testUser"));
+        AuthToken token = response.getAuthToken();
         postService = new PostServiceProxy();
-        goodRequest = new PostRequest("This is a status", "dummyUser", new AuthToken("Authorized"));
+        goodRequest = new PostRequest("This is a status", "testUser", token);
         badRequest = new PostRequest("This is a bad status", "badUser", new AuthToken());
     }
 
